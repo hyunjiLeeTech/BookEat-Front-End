9c3484752a3531f866307cbe823e3344f674b008
hyouksunkwon <ddauppo@gmail.com>
2020-06-13 06:40:17 -0400

    LOGIN

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react'
-import SignUp from './component/Forms/SignUp'
-import { Route, Switch } from 'react-router-dom'
+import React, { Component } from "react";
+import SignUp from "./component/Forms/SignUp";
+import { Route, Switch } from "react-router-dom";
+import LoginPageTest from "./component/Forms/LoginPageTest";
 
 class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path='/' render={() => (
-          <SignUp />
-        )} />
-        <Route exact path='/SignUp' render={() => (
-          <SignUp />
-        )} />
-      
+        <Route exact path="/" render={() => <LoginPageTest />} />
+        <Route exact path="/SignUp" render={() => <SignUp />} />
+        <Route exact path="/LoginPageTest" render={() => <LoginPageTest />} />
       </Switch>
-    )
+    );
   }
 }
 
--- src/component/Style/NavBar.js
+++ src/component/Style/NavBar.js
@@ -1,36 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './NavBar.css'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 const NavBar = () => {
-
-    return (
-
-        <nav className="navbar navbar-default navbar-fixed-top">
-            <div className="topbar">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <Link className="navbar-brand" to="/">BookEat</Link>
-                    </div>
-                    <ul className="nav navbar-nav navbar-right">
-                        <li><Link className="glyphicon glyphicon-user" to="/SignUp">Sign Up</Link></li>
-                        <li><Link className="glyphicon glyphicon-log-in" to="/">Login</Link></li>
-                    </ul>
-                </div>
-            </div>
-            <div className="lowerbar" >
-                <ul className="nav navbar-nav" >
-                    <li> <Link className="navbar-brand" to="/">Home</Link> </li>
-                </ul>
-            </div>
-
-        </nav>
-
-
-    )
-
-
-}
+  return (
+    <nav className="navbar navbar-default navbar-fixed-top">
+      <div className="topbar">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <Link className="navbar-brand" to="/">
+              BookEat
+            </Link>
+          </div>
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <Link className="glyphicon glyphicon-user" to="/SignUp">
+                Sign Up
+              </Link>
+            </li>
+            <li>
+              <Link className="glyphicon glyphicon-log-in" to="/">
+                Login
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="lowerbar">
+        <ul className="nav navbar-nav">
+          <li>
+            {" "}
+            <Link className="navbar-brand" to="/">
+              Home
+            </Link>{" "}
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
