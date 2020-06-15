52845c3b4d577e32210c2c739765b7d4a605c453
Shichao Wang <swang206@myseneca.ca>
2020-06-14 22:05:21 -0400

    validatation update

--- src/component/Forms/Customer/SignUp.js
+++ src/component/Forms/Customer/SignUp.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom'
 //import GoogleSignIn from 'react-google-signin'
 import MainContainer from '../../Style/MainContainer'
 import './SignUp.css'
-
+import Parser from 'html-react-parser'
+import $ from 'jquery'
 
 //Validation 
 const regExpEmail = RegExp(
@@ -47,12 +48,12 @@ class SignUp extends Component {
       password: '',
       confirmpw: '',
       isError: {
-        firstname: '',
-        lastname: '',
-        email: '',
-        phonenumber: '',
-        password: '',
-        confirmpw: ''
+        firstname: '&#160;',
+        lastname: '&#160;',
+        email: '&#160;',
+        phonenumber: '&#160;',
+        password: '&#160;',
+        confirmpw: '&#160;'
       }
     };
 
@@ -64,33 +65,38 @@ class SignUp extends Component {
     e.preventDefault();
     const { name, value } = e.target;
     let isError = { ...this.state.isError };
-
+    //console.log(name);
+    //console.log(value);
     switch (name) {
       case "firstname":
         isError.firstname =
-          value.length >= 1 && value.length <= 32 ? "Atleast 1 character required" : "";
+          value.length >= 1 && value.length <= 32 ?  "&#160;" : "Atleast 1 character required";
         break;
       case "lastname":
         isError.lastname =
-          value.length >= 1 && value.length <= 32 ? "Atleast 1 character required" : "";
+          value.length >= 1 && value.length <= 32 ? "&#160;" : "Atleast 1 character required";
         break;
       case "email":
         isError.email = regExpEmail.test(value)
-          ? ""
+          ? "&#160;"
           : "Email address is invalid";
         break;
         case "phonenumber":
         isError.phonenumber = regExpPhone.test(value)
-          ? "" : "Phone Number is invalid";
+          ? "&#160;" : "Phone Number is invalid";
         break;
       case "password":
         isError.password =
-          value.length >= 6 && value.length <= 32  ? "Atleast 6 characters required" : "";
+          value.length >= 6 && value.length <= 32  ? "&#160;" : "Atleast 6 characters required"
+        //console.log(value.length);
+        this.state.password = value;
         break;
       case "confirmpw":
-          isError.confirmpw =
-            value.length >= 6 && value.length <= 32  ? "Atleast 6 characters required" : "";
-          break;
+        this.state.confirmpw = value;
+        isError.confirmpw =
+        this.state.confirmpw === this.state.password  ? "&#160;" : "Password not matching"
+        console.log(this.state.confirmpw === this.state.password);
+        break;
       default:
         break;
     }
@@ -129,6 +135,9 @@ class SignUp extends Component {
       if (event.keyCode === 32) return false;
     }
 
+    $("#conditionbtn").on("click", ()=>{
+      $("#accept-terms").removeAttr("disabled");
+    })
     // var t2 = document.getElementById("lastname");
     // t2.onkeypress = function (e) {
     //   if (e.keyCode === 32) return false;
@@ -156,7 +165,7 @@ class SignUp extends Component {
                   <input type="text" id="firstname" name="firstname" value={this.state.firstname} placeholder="First Name"
                     className={isError.firstname.length > 0 ? "is-invalid form-control" : "form-control"} onChange={this.handleChange} required />
                     {isError.firstname.length > 0 && (
-                        <span className="invalid-feedback">{isError.firstname}</span>
+                        <span className="invalid-feedback">{Parser(isError.firstname)}</span>
                     )}
                 </div>
               </div>
@@ -164,10 +173,10 @@ class SignUp extends Component {
               <div className="form-group row">
                 <label htmlFor="lastname" className="col-sm-2 col-form-label">Last Name </label>
                 <div className="col-sm-6">
-                  <input type="text" id="lastname" value={this.state.lastname} placeholder="Last Name"
+                  <input type="text" id="lastname" name="lastname" value={this.state.lastname} placeholder="Last Name"
                     className={isError.lastname.length > 0 ? "is-invalid form-control" : "form-control"} onChange={this.handleChange}  required />
                     {isError.lastname.length > 0 && (
-                        <span className="invalid-feedback">{isError.lastname}</span>
+                        <span className="invalid-feedback">{Parser(isError.lastname)}</span>
                     )}
                 </div>
               </div>
@@ -175,10 +184,10 @@ class SignUp extends Component {
               <div className="form-group row">
                 <label htmlFor="email" className="col-sm-2 col-form-label"> Email </label>
                 <div className="col-sm-6">
-                  <input type="email" id="email" className={isError.email.length > 0 ? "is-invalid form-control" : "form-control"} value={this.state.email} placeholder="Email"
+                  <input type="email" name='email' id="email" className={isError.email.length > 0 ? "is-invalid form-control" : "form-control"} value={this.state.email} placeholder="Email"
                     onChange={this.handleChange} required />
                     {isError.email.length > 0 && (
-                        <span className="invalid-feedback">{isError.email}</span>
+                        <span className="invalid-feedback">{Parser(isError.email)}</span>
                     )}
                 </div>
               </div>
@@ -187,21 +196,21 @@ class SignUp extends Component {
               <div className="form-group row">
                 <label htmlFor="phonenumber" className="col-sm-2 col-form-label">Phone Number </label>
                 <div className="col-sm-6">
-                  <input type="text" id="phonenumber" className={isError.phonenumber.length > 0 ? "is-invalid form-control" : "form-control"} value={this.state.phonenumber} placeholder="Phone Number"
+                  <input type="number" id="phonenumber" className={isError.phonenumber.length > 0 ? "is-invalid form-control" : "form-control"} value={this.state.phonenumber} placeholder="Phone Number"
                     onChange={this.handleChange}  required />
                     {isError.phonenumber.length > 0 && (
-                        <span className="invalid-feedback">{isError.phonenumber}</span>
+                        <span className="invalid-feedback">{Parser(isError.phonenumber)}</span>
                     )}
                 </div>
               </div>
 
               <div className="form-group row">
-                <label htmlFor="password" className="col-sm-2 col-form-label">Password </label>
+                <label htmlFor="password"  className="col-sm-2 col-form-label">Password </label>
                 <div className="col-sm-6">
-                  <input type="password" id="password" className={isError.password.length > 0 ? "is-invalid form-control" : "form-control"} value={this.state.password} placeholder="Password"
+                  <input name="password" type="password" id="password" className={isError.password.length > 0 ? "is-invalid form-control" : "form-control"} value={this.state.password} placeholder="Password"
                     onChange={this.handleChange} required />
                     {isError.password.length > 0 && (
-                        <span className="invalid-feedback">{isError.password}</span>
+                        <span className="invalid-feedback">{Parser(isError.password)}</span>
                     )}
                 </div>
               </div>
@@ -209,19 +218,19 @@ class SignUp extends Component {
               <div className="form-group row">
                 <label htmlFor="confirmpw" className="col-sm-2 col-form-label">Password Confirmation </label>
                 <div className="col-sm-6">
-                  <input type="password" id="confirmpw" className={isError.confirmpw.length > 0 ? "is-invalid form-control" : "form-control"} value={this.state.confirmpw} placeholder="Confirm Password"
+                  <input type="password" name="confirmpw" id="confirmpw" className={isError.confirmpw.length > 0 ? "is-invalid form-control" : "form-control"} value={this.state.confirmpw} placeholder="Confirm Password"
                     onChange={this.handleChange}  required />
                     {isError.confirmpw.length > 0 && (
-                        <span className="invalid-feedback">{isError.confirmpw}</span>
+                        <span className="invalid-feedback">{Parser(isError.confirmpw)}</span>
                     )}
                 </div>
               </div>
 
               <div className="form-group ">
                 <div className="form-check">
-                  <input type="checkbox" id="accept-terms" className="form-check-input" required />
-                  <label htmlFor="accept-terms" className="form-check-label">Accept Terms &amp; Conditions</label>
+                  <input type="checkbox" id="accept-terms" disabled className="form-check-input" required />
                 </div>
+                <label htmlFor="accept-terms" className="form-check-label">Accept <a id="conditionbtn" target="_blank" href="https://google.ca">Terms &amp; Conditions</a></label>
               </div>
               <div className="text-center">
                 <button type="submit" className="btn btn-primary" >Sign Up</button>
