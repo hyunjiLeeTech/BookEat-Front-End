d7ceeafde8b5c336f77343d9b9bc2a9c541efa7e
Shichao Wang <swang206@myseneca.ca>
2020-07-11 22:12:49 -0400

    heroku pushing

--- src/Services/ServerUrl.js
+++ src/Services/ServerUrl.js
@@ -1,3 +1,3 @@
-const serverAddress = process.env.SERVERADDRESS || "http://localhost:5000";
-//const serverAddress = 'http://bookeatback.herokuapp.com/'
+//const serverAddress = process.env.SERVERADDRESS || "http://localhost:5000";
+const serverAddress = 'http://bookeatback.herokuapp.com/'
 export default serverAddress;
