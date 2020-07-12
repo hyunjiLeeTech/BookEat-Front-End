bef84947425939b84c42ea2ce437e39a1b42ad3f
Shichao Wang <swang206@myseneca.ca>
2020-07-11 22:38:49 -0400

    heroku url update

--- src/Services/ServerUrl.js
+++ src/Services/ServerUrl.js
@@ -1,3 +1,3 @@
 //const serverAddress = process.env.SERVERADDRESS || "http://localhost:5000";
-const serverAddress = 'https://bookeatback.herokuapp.com/'
+const serverAddress = 'https://bookeatback.herokuapp.com'
 export default serverAddress;
