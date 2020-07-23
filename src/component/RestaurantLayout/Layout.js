6e63af5bd335c4a4488a45749d296d1cf61bc672
Shichao Wang <swang206@myseneca.ca>
2020-07-23 17:50:51 -0400

    layout update

--- src/component/RestaurantLayout/Layout.js
+++ src/component/RestaurantLayout/Layout.js
@@ -7,11 +7,16 @@ import $ from 'jquery';
 import Carousel from 'react-bootstrap/Carousel'
 
 class Layout extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        if(props.tables){
+        if (props.tables) {
             this.tables = props.tables;
+            this.state = {
+                carouselIndex: 0,
+
+            }
         }
+        this.genCarousel = this.genCarousel.bind(this);
     }
 
     componentWillMount() {
@@ -34,101 +39,134 @@ class Layout extends Component {
             //{ id: 115, rtid: 9, restaurantId: 1, isOpen: false, capacity: 9, prefers: "" },
             { id: 119, rtid: 10, restaurantId: 1, isOpen: false, capacity: 9, prefers: "kitchen" },
         ]
-        if(!this.tables)
+        if (!this.tables)
             this.tables = tables;
-        else{
+        else {
             tables = this.tables;
         }
 
         var rowshtmls = [];
-        var tableCount = tables.length > 9 ? 9 : tables.length;
-        for (var i = 0; i < tableCount / 3; i++) {
-            var row = [];
-            for (var j = 0; j < 3; j++) {
-                if ((3 * i + j) >= tableCount) continue;
-                var theTable = tables[3 * i + j];
-                var cn = theTable.prefers + " r-table col-md-4 hovereffect" + (theTable.isOpen ? " open" : " close");
-                var tableImg = table_s;
-                if (theTable.capacity <= 4) tableImg=table_s;
-                else if(theTable.capacity <= 7) tableImg=table_m;
-                else if(theTable.capacity <= 10) tableImg=table_l;
-                row.push(
-                    <div key={theTable.id} className={cn + " r-table-s"} id={(3 * i + j)}>
-                        <img src={tableImg} className={theTable.isOpen ? " img-responsive" : ""} />
-                        {theTable.isOpen ?
-                            <div class="overlay">
-                                <h2>Click to Reserve</h2>
-                            </div> : <div>
-                                <div class="overlay">
-                                    <p className="info">Unavalible</p>
-                                </div>
-                            </div>}
-                    </div>
-                )
+        // var tableCount = tables.length > 9 ? 9 : tables.length;
+        // for (var i = 0; i < tableCount / 3; i++) {
+        //     var row = [];
+        //     for (var j = 0; j < 3; j++) {
+        //         if ((3 * i + j) >= tableCount) continue;
+        //         var theTable = tables[3 * i + j];
+        //         var cn = theTable.prefers + " r-table col-md-4 hovereffect" + (theTable.isOpen ? " open" : " close");
+        //         var tableImg = table_s;
+        //         if (theTable.capacity <= 4) tableImg=table_s;
+        //         else if(theTable.capacity <= 7) tableImg=table_m;
+        //         else if(theTable.capacity <= 10) tableImg=table_l;
+        //         row.push(
+        //             <div key={theTable.id} className={cn + " r-table-s"} id={(3 * i + j)}>
+        //                 <img src={tableImg} className={theTable.isOpen ? " img-responsive" : ""} />
+        //                 {theTable.isOpen ?
+        //                     <div className="overlay">
+        //                         <h2>Click to Reserve</h2>
+        //                     </div> : <div>
+        //                         <div className="overlay">
+        //                             <p className="info">Unavalible</p>
+        //                         </div>
+        //                     </div>}
+        //             </div>
+        //         )
 
-            }
-            rowshtmls.push(<div key={"r" + i} className="row">
-                {row}
-            </div>);
+        //     }
+        //     rowshtmls.push(<div key={"r" + i} className="row">
+        //         {row}
+        //     </div>);
+        // }
+        var tableCount = tables.length;
+        var row = [];
+        for (var i in tables) {
+            var theTable = tables[i];
+            var cn = theTable.prefers + " r-table col-md-4 hovereffect animated bounce" + (theTable.isOpen ? " open" : " close");
+            var tableImg = table_s;
+            if (theTable.capacity <= 4) tableImg = table_s;
+            else if (theTable.capacity <= 7) tableImg = table_m;
+            else if (theTable.capacity <= 10) tableImg = table_l;
+            row.push(
+                <div key={theTable._id} className={cn + " r-table-s"} id={i}>
+                    <img src={tableImg} className={theTable.isOpen ? " img-responsive" : ""} />
+                    {theTable.isOpen ?
+                        <div className="overlay">
+                            <h2>Click to Reserve</h2>
+                        </div> : <div>
+                            <div className="overlay">
+                                <p className="info">Unavalible</p>
+                            </div>
+                        </div>}
+                </div>
+            )
         }
-        this.rowhtmls = rowshtmls;
+
+        //this.rowhtmls = rowshtmls;
+        this.rows = row;
     }
 
     genCarousel(htmls) {
-        const [index, setIndex] = useState(0);
-      
         const handleSelect = (selectedIndex, e) => {
-          setIndex(selectedIndex);
+            this.setState({ carouselIndex: selectedIndex });
         };
-        
+        var rows = []; //a list of row that contains 3 itmes 
+        var tempItems = []; // a list of items. usually no more than 3;         
+        for (var i in htmls) { //generate row
+            i = Number.parseInt(i)
 
+            if (i !== 0 && i % 3 === 0) {
+                rows.push(<div key={"r" + rows.length} className="row">
+                    {tempItems}
+                </div>)
+                tempItems = [];
+            }
+            tempItems.push(htmls[i])
+        }
+        if (tempItems.length !== 0) {
+            rows.push(<div key={"r" + rows.length} className="row">
+                {tempItems}
+            </div>)
+            tempItems = [];
+        }
+
+
+
+        var slides = [];
+        var tempRows = [];
+
+
+        for (var i in rows) { //generate slides
+            i = Number.parseInt(i)
+            if (i !== 0 && i % 3 === 0) {
+                slides.push(<Carousel.Item key={'slide'+slides.length}>
+                    <div style={{marginLeft: '15%', marginRight: '15%'}}>
+                        {tempRows}
+                    </div>
+                </Carousel.Item>)
+                tempRows = new Array();
+            }
+            tempRows.push(rows[i])
+        }
+
+
+        if (tempRows.length !== 0) {
+            slides.push(<Carousel.Item  key={'slide'+slides.length}>
+                <div style={{marginLeft: '15%', marginRight: '15%'}} >
+                    {tempRows}
+                </div>
+            </Carousel.Item>)
+            tempRows = [];
+        }
 
         return (
-          <Carousel activeIndex={index} onSelect={handleSelect}>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=First slide&bg=373940"
-                alt="First slide"
-              />
-              <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=Second slide&bg=282c34"
-                alt="Second slide"
-              />
-      
-              <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-              <img
-                className="d-block w-100"
-                src="holder.js/800x400?text=Third slide&bg=20232a"
-                alt="Third slide"
-              />
-      
-              <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>
-                  Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                </p>
-              </Carousel.Caption>
-            </Carousel.Item>
-          </Carousel>
+            <Carousel activeIndex={this.state.carouselIndex} indicators={true} interval={null} onSelect={handleSelect}>
+                {slides}
+            </Carousel>
         );
-      }
-      
+    }
 
 
-    
+
+
 
 
 
@@ -146,6 +184,10 @@ class Layout extends Component {
             reactThis.props.setTableId(reactThis.tables[id]._id);
             this.selectedTable = reactThis.tables[id];
         })
+        var row = $('.carousel-item div.row img');
+        row.first().one('load', function(){
+            $('.carousel-item').css('min-height', $(this).height()*3+'px');
+        })
 
     }
 
@@ -153,8 +195,8 @@ class Layout extends Component {
 
     render() {
         return (
-            <div className="layoutwrapper">
-                {this.rowhtmls}
+            <div className="layoutwrapper" style={{background:'#aaaaaa'}}>
+                {this.genCarousel(this.rows)}
             </div>
         )
     }
