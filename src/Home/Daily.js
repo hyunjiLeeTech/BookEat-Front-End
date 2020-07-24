b8cda05dad7201a3fbd470a74302a8520187e9d9
ddauppo@gmail.com <ddauppo@gmail.com>
2020-07-24 14:31:08 -0400

    daily

--- src/Home/Daily.js
+++ src/Home/Daily.js
@@ -1,11 +1,49 @@
 import React, { Component } from 'react'
+import MainContainer from '../component/Style/MainContainer';
+import $ from "jquery";
+import { Link } from 'react-router-dom'
+import CAFE from '../Image/CAFE.jpg'
 
 
 class Daily extends Component {
 
-    render(){
-        return(
-            <h5>Daily Pick Up</h5>
+    constructor(props) {
+        super(props)
+        // this.state = {
+        //     restaurant: [ {id:"", restaurantName:"", restaurantPicture:"", totalRate:"" },
+        //     {id:"", restaurantName:"", restaurantPicture:"", totalRate:"" },
+        //     {id:"", restaurantName:"", restaurantPicture:"", totalRate:"" },
+        //     {id:"", restaurantName:"", restaurantPicture:"", totalRate:"" }]
+
+        //restaurants: []    }
+
+        // renderRestaurants(restaurants) {
+
+        // }
+
+    }
+
+    render() {
+        return (
+            <MainContainer>
+                <h5>Daily Pick Up</h5>
+                <div class="card-deck">
+                    <div class="card">
+                        <img class="card-img-top" src={CAFE.jpg} alt="Card image cap" />
+                        <div class="card-body">
+                            <h5 class="card-title">restaurantName</h5>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <img class="card-img-top" src=".../100px180/" alt="Card image cap" />
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>                    
+                </div>
+            </MainContainer>
         )
     }
 }
