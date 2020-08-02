87fb604e0a1bb05071480400a663d2f37f3de53d
ddauppo@gmail.com <ddauppo@gmail.com>
2020-08-02 15:11:24 -0400

    review

--- src/component/Forms/Logout.js
+++ src/component/Forms/Logout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom'
+// import { NavLink } from 'react-router-dom'
 import authService from '../../Services/AuthService';
 import './Logout.css'
 import MainContainer from '../Style/MainContainer';
