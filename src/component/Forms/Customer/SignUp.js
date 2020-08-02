8f1a158917c69f5b6c69fa8a7bb9f89b806fb305
Elisa <elisangli11@gmail.com>
2020-08-02 12:38:25 -0400

    Change modal text

--- src/component/Forms/Customer/SignUp.js
+++ src/component/Forms/Customer/SignUp.js
@@ -131,7 +131,7 @@ class SignUp extends Component {
       Axios.post(serverAddress + "/customersignup", this.state).then(res => {
         console.log(res)
         if (res.data.errcode === 0) {
-          $("#signResultText").text("Congrats, You can now log into BookEat using your account").removeClass("alert-warning").removeClass("alert-danger").removeClass("alert-success")
+          $("#signResultText").text("Congrats, check your email to confirm that you are human").removeClass("alert-warning").removeClass("alert-danger").removeClass("alert-success")
             .addClass("alert-success");
         } else {
           $("#signResultText").text("Sorry, " + res.data.errmsg).removeClass("alert-warning").removeClass("alert-danger").removeClass("alert-success")
