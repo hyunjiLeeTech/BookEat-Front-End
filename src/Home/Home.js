dd9061f90ef75df8acb8b44deb77d2bae3af98f8
Elisa <elisangli11@gmail.com>
2020-08-02 13:05:23 -0400

    remove stars code

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -5,40 +5,13 @@ import Feature from './Feature';
 import Favorite from './Favorite';
 import SearchBox from './SearchBox';
 // import FullscreenError from '../component/Style/FullscreenError'
-// import $ from "jquery";
 
-// import Star from '../component/Style/Stars'
+
 class Home extends Component {
-    
+
     render() {
         return (
             <MainContainer>
-
-                {/* <div style={{ width: '20%' }}> */}
-                    {/* type: can be star, btns, splitedBar, bar
-                    bar: unclickable, can display float value
-                    splitedBar: unclickable, can display float value
-                    star: clickable or unclickable, cannot display float value
-                    btns: clickable or unclickable, cannot display float value
-                stars: number 1-5
-                callback: (StarClicked) => void starClicked is what user clicked
-                 */}
-                    {/* <Star type="star"/>
-                    <hr />
-                    <Star type='bar' stars={3.8} />
-                    <hr />
-                    <Star type='splitedBar' stars={3.8} />
-                    <hr />
-                    <Star isClickAble={true} type='star' stars={2} callback={
-                        (e) => { console.log(e) }
-                    } />
-                    <hr />
-                    <Star isClickAble={true} type='btns' stars={4} callback={
-                        (e) => { console.log(e) }
-                    } />
-                </div> */}
-
-
                 <div className="container">
                     <header className="jumbotron my-4">
                         <h1 className="display-3">Welcome to BookEat!</h1>
@@ -49,24 +22,24 @@ class Home extends Component {
 
                     </header>
                     <div className="card " >
-                        <div className="card-body" >    
-                        <h5>Daily Pick Up Restaurant</h5>                      
-                                <Daily />
+                        <div className="card-body" >
+                            <h5>Daily Pick Up Restaurant</h5>
+                            <Daily />
                         </div>
                     </div>
 
-                   
+
                     <br></br>
                     <div className="card" >
                         <div className="card-body">
-                        <h5>Feature Restaurant</h5>      
+                            <h5>Feature Restaurant</h5>
                             <Feature />
                         </div>
                     </div>
                     <br></br>
                     <div className="card" >
                         <div className="card-body">
-                        <h5>Favorite Restaurant</h5>      
+                            <h5>Favorite Restaurant</h5>
                             <Favorite />
                         </div>
                     </div>
