e64bf6f15bf76a28f930d444946ec85cc10763bb
Elisa <elisangli11@gmail.com>
2020-08-05 19:57:47 -0400

    fix tabIndex

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -538,7 +538,7 @@ class Menu extends Component {
                 <div
                     className="modal fade"
                     id="menuAddResultModal"
-                    tabindex="-1"
+                    tabIndex="-1"
                     role="dialog"
                     aria-labelledby="menuAddResultModal"
                     aria-hidden="true"
@@ -582,7 +582,7 @@ class Menu extends Component {
                 <div
                     className="modal fade"
                     id="EditResultModal"
-                    tabindex="-1"
+                    tabIndex="-1"
                     role="dialog"
                     aria-labelledby="EditResultModal"
                     aria-hidden="true"
@@ -626,7 +626,7 @@ class Menu extends Component {
                 <div
                     className="modal fade"
                     id="DeleteResultModal"
-                    tabindex="-1"
+                    tabIndex="-1"
                     role="dialog"
                     aria-labelledby="DeleteResultModal"
                     aria-hidden="true"
