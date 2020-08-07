18b440109da8c5d5ea09708ba78eb0d0e9aff0e7
HyunjiLee <hyunji.lee.tech@gmail.com>
2020-08-07 01:17:47 -0400

    Changed the way of using func edit cus profile

--- src/Services/dataService.js
+++ src/Services/dataService.js
@@ -424,8 +424,7 @@ export default {
     Axios.post(serverAddress + "/updatecustomerinfo", state, {
       headers: authHeader(),
     }).then((res) => {
-      console.log(res);
-
+      return res.data;
     }).catch(err => {
       if (err.response && err.response.status === 401) {
         window.location.href = '/error?Hint=Permission Denied(updateCustomerInformation)&message=Your permision is denied, may be your account has been logged in on another device, please login again&foreceLogout=true'
