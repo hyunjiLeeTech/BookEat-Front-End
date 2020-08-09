76b8bea58ca74e2dbe4a7236497eb4fde3b033d9
Elisa <elisangli11@gmail.com>
2020-08-08 20:22:26 -0400

    Change NavBar color

--- src/component/Style/NavBar.js
+++ src/component/Style/NavBar.js
@@ -3,7 +3,9 @@ import { NavLink } from 'react-router-dom'
 import NavBarRightLoggedin from './NavBarRight-loggedin';
 import NavBarRightLoggedOut from './NavBarRight-loggedout';
 import authService from '../../Services/AuthService';
-import BookEat_logo from '../../Image/BookEat_logo-03.jpg'
+import BookEat_logo from '../../Image/BookEat_logo-03.png';
+import BookEat_logo_r from '../../Image/BookEat_logo-04.png';
+
 const NavBar = () => {
     const user = authService.getCurrentUser();
 
@@ -25,33 +27,14 @@ const NavBar = () => {
         }
     }
 
-    return (
-        // <nav className="navbar navbar-default navbar-fixed-top">
-        //     <div className="topbar">
-        //         <div className="container-fluid">
-        //             <div className="navbar-header">
-        //                 <Link className="navbar-brand" to="/">BookEat</Link>
-        //                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        //                     <span className="navbar-toggler-icon"></span>
-        //                 </button>
-        //             </div>
-        //             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        //                 <ul className="nav navbar-nav navbar-right">
-        //                     <li className="nav-item"><Link className="glyphicon glyphicon-user" to="/SignUp">Sign Up</Link></li>
-        //                     <li className="nav-item"><Link className="glyphicon glyphicon-log-in" to="/">Login</Link></li>
-        //                 </ul>
-        //             </div>
-        //         </div>
-        //     </div>
-        //     <div className="lowerbar bg-muted" >
-        //         <ul className="nav navbar-nav bg-muted" >
-        //             <li className="nav-item"> <Link className="navbar-brand" to="/">Home</Link> </li>
-        //         </ul>
-        //     </div>
-        // </nav>
+    var u = authService.getCurrentUser().user;
 
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <NavLink to="/" className="nav-link"> BookEat_logo</NavLink>
+    if(u.userTypeId === 1){
+        return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-info">
+            <NavLink to="/" className="nav-link"> 
+            <img src={BookEat_logo} alt="BookEat" height="28"/>
+            </NavLink>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
@@ -59,16 +42,11 @@ const NavBar = () => {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
-                        {/* <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a> */}
                         <NavLink to="/" className="nav-link">Home</NavLink>
                     </li>
                     <li className="nav-item">
                         {renderProfileButtom()}
                     </li>
-
-                    {/* <li className="nav-item">
-                        <a className="nav-link disabled" href="#">Disabled</a>
-                    </li> */}
                 </ul>
 
                 {user === null ? <NavBarRightLoggedOut /> : <NavBarRightLoggedin />}
@@ -76,6 +54,58 @@ const NavBar = () => {
             </div>
         </nav>
     )
+    }else if (u.userTypeId === 2 || u.userTypeId === 3){
+        return (
+            <nav className="navbar navbar-expand-lg navbar-light bg-danger">
+                <NavLink to="/" className="nav-link"> 
+                <img src={BookEat_logo_r} alt="BookEat" height="28"/>
+                </NavLink>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+    
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <NavLink to="/" className="nav-link">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            {renderProfileButtom()}
+                        </li>
+                    </ul>
+    
+                    {user === null ? <NavBarRightLoggedOut /> : <NavBarRightLoggedin />}
+    
+                </div>
+            </nav>
+        )
+    }else {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <NavLink to="/" className="nav-link"> 
+                <img src={BookEat_logo} alt="BookEat" height="28"/>
+                </NavLink>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+    
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item">
+                            <NavLink to="/" className="nav-link">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            {renderProfileButtom()}
+                        </li>
+                    </ul>
+    
+                    {user === null ? <NavBarRightLoggedOut /> : <NavBarRightLoggedin />}
+    
+                </div>
+            </nav>
+        )
+    }
+    
 }
 
 export default NavBar;
