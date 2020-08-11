a6e3598e93bc38185c901451d581b6c2f3e9bc59
Elisa <elisangli11@gmail.com>
2020-08-10 22:34:29 -0400

    Added Delete restaurant modal

--- src/component/Profile/Restaurant/RestaurantProfile.js
+++ src/component/Profile/Restaurant/RestaurantProfile.js
@@ -1894,6 +1894,48 @@ class RestaurantProfile extends Component {
                 </div>
               </div>
 
+              {/* Delete Restaurant Modal */}
+              <div
+                className="modal fade"
+                id="deleteRestaurantModal"
+                tabIndex="-1"
+                role="dialog"
+                aria-labelledby="ddeleteRestaurantModalLabel"
+                aria-hidden="true"
+              >
+                <div className="modal-dialog" role="document">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <h5 className="modal-title" id="deleteRestaurantModalLabel">
+                        Delete Restaurant Profile
+                </h5>
+                      <button
+                        type="button"
+                        className="close"
+                        data-dismiss="modal"
+                        aria-label="Close"
+                      >
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div className="modal-body">
+                      <p className="alert alert-warning" id="deleteRestaurantModalText">
+                        Please Wait...
+                </p>
+                    </div>
+                    <div className="modal-footer">
+                      <button
+                        type="button"
+                        className="btn btn-primary"
+                        data-dismiss="modal"
+                      >
+                        Close
+                </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
